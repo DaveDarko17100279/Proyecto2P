bb96477cd7a5a60d8796c9f7a1763da4a0f403af
Samuel Padilla <a17100255@ceti.mx>
2020-11-02 19:06:03 -0600

    Puse un System.out.print
    dice Hola,buenos días.

--- src/proyecto2p/Proyecto2P.java
+++ src/proyecto2p/Proyecto2P.java
@@ -16,6 +16,8 @@ public class Proyecto2P {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.print("Hola, buenos días");
     }
     
 }
