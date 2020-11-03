9ddb15cae807a499cb06f0f316927d98063f022b
AndyJmnzEsp <a17100657@ceti.mx>
2020-11-03 16:38:31 -0600

    Index Verion hoy
    Al cienon

--- src/Views/index.java
+++ src/Views/index.java
@@ -1,18 +1,25 @@
 package Views;
 
+import Formatos.Botones;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Insets;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+
 
 public class index extends JFrame {
     
     JPanel Botones  = new JPanel();
-    JButton login = new JButton("INICIAR SESION");
-    JButton registro = new JButton("REGISTRO");
+    JButton login = new Botones("LOGIN");
+    JButton registro = new Botones("CREATE ACCOUNT");
     FondoPanel fondo = new FondoPanel();
     
     
@@ -22,7 +29,7 @@ public class index extends JFrame {
         //tamaño de la ventana
         setSize(1000,800);
         //Se podra redimencionar
-        //setResizable(false);
+        setResizable(false);
         //Localizacion
         setLocationRelativeTo(null);
         //Salir
@@ -32,33 +39,36 @@ public class index extends JFrame {
         //Crear la interfaz
         init();
         transparenciaButton();
-        //this.getContentPane().setBackground(new java.awt.Color(38, 70, 83));
         
     }
      private void init() {
         // Layout 
         this.setLayout(null);
+        BordeRedondo border = new BordeRedondo(40); // radio = 10
         
         Botones.setLayout(null);
-        Botones.setBounds(300, 220, 400, 300);
-        //Botones.setBackground(new java.awt.Color(233, 196, 106));
+        Botones.setBounds(300, 250, 400, 300);
+        Botones.setBackground(new java.awt.Color(233, 196, 106));
         
         
         login.setBounds(50, 70, 300, 45);
         login.setFont(new java.awt.Font("Segoe UI Light", 1, 18));
         login.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        login.setBackground(new java.awt.Color(233, 196, 106));
+        login.setBorder(border);
+        login.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         
         registro.setBounds(50, 185, 300, 45);
         registro.setFont(new java.awt.Font("Segoe UI Light", 1, 18));
         registro.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        registro.setBackground(new java.awt.Color(233, 196, 106));
+        registro.setBorder(border);
+        registro.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         
         Botones.add(login);
         Botones.add(registro);
         getContentPane().add(Botones);
         
     }
+     
     class FondoPanel extends JPanel {
         private Image imagen;
          
@@ -73,10 +83,35 @@ public class index extends JFrame {
             super.paint(g);
         }
     }
+    
     public void transparenciaButton() {
+        login.setOpaque(false);
+        login.setContentAreaFilled(false);
+        login.setBorderPainted(true);
         
-        
-        Botones.setOpaque(false);
-        
+        registro.setOpaque(false);
+        registro.setContentAreaFilled(false);
+        registro.setBorderPainted(true);
+    }
+    
+    class BordeRedondo implements Border {
+
+        private int radio;  
+
+        BordeRedondo(int radius) {
+            this.radio = radius;
+        }  
+
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radio+1, this.radio+1, this.radio+2, this.radio);
+        }  
+
+        public boolean isBorderOpaque() {
+            return true;
+        }  
+
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.drawRoundRect(x, y, width-1, height-1, radio, radio);
+        }
     }
 }
