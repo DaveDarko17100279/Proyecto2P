c4ede33c52fe552438baef70884cdd61725edf53
AldoRdzarias <a17100291@live.ceti.mx>
2020-11-03 08:14:00 -0600

    2

--- src/BD/conexion.java
+++ src/BD/conexion.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package BD;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author aldor
- */ 
 public class conexion {
      private static Connection con = null;
     private static final String driver ="com.mysql.jdbc.Driver";
--- src/proyecto2p/Proyecto2P.java
+++ src/proyecto2p/Proyecto2P.java
@@ -1,36 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto2p;
 
 import BD.conexion;
 import Views.index;
 import java.sql.Connection;
 
-/** Hola amikos de yutudwdaawdwada w 
- *
- * @author DaveDarko
- */
+
 public class Proyecto2P {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         conexion conex = new conexion();
         Connection cn = conex.getConnection();
-        System.out.println("Login");
-        System.out.println("Hola, buenos días");
-        System.out.print("Hola, buenos días");
-        System.out.print("Hola, buenos días Soy Rangel");
-        System.out.print("Eduardo Canadiense");
 
          new index().setVisible(true);
-         //Hola Mundo Cómo estas
-        //Hola Mundo da
     }
     
 }
