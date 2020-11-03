1fa641355e693388b7e72ecb8e48a0fe969ab426
AldoRdzarias <a17100291@live.ceti.mx>
2020-11-02 19:33:29 -0600

    Conexion

--- src/BD/conexion.java
+++ src/BD/conexion.java
@@ -5,10 +5,40 @@
  */
 package BD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author aldor
- */
+ */ 
 public class conexion {
+     private static Connection con;
+    private static final String driver ="com.mysql.jdbc.Driver";
+    private static final String user = "root";
+    private static final String password = "";
+    private static final String url= "jdbc:mysql://localhost:3306/conferencia";
+
+    public conexion() {
+        con = null;
+        try{
+            Class.forName(driver);
+            con = DriverManager.getConnection(url,user,password);
+            if(con != null){
+                System.out.println("Conexion exitosa");
+            }
+        }catch (ClassNotFoundException | SQLException e){
+            
+        }
+    }
     
+    //conexion a la BD
+    public Connection getConnection(){
+        return con;
+    }
+    //desconectar la bd
+    public void desconectar(){
+        con = null;
+    }
 }
