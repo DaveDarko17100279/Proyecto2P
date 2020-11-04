29fe50bf3eb1194b41b28dd29e0b0eb40c2bcd61
AndyJmnzEsp <a17100657@ceti.mx>
2020-11-03 20:41:03 -0600

    Pa poner cosas en hint
    Ya no andamos al cienon :(

---
+++ src/Formatos/JTextFieldHint.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Formatos;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author alex_
+ */
+
+public class JTextFieldHint  extends JTextField implements FocusListener{
+    private final Font fontLost = new Font("Monaco",Font.BOLD, 16);
+    private final Font fontGained = new Font("Monaco", Font.BOLD,16);
+    private final Color colorLost = Color.LIGHT_GRAY;
+    private Color colorGained = Color.BLACK;
+    private String hint;
+    
+    public JTextFieldHint(){
+        addFocusListener(this);
+    }
+    public void setHint(String hint) {
+        setForeground(colorLost);
+        setFont(fontLost);
+        setText(hint);
+        this.hint = hint;
+    }
+    public String getHint(){
+        return hint;
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        if (getText().equals(getHint())) {
+            setText("");
+            setFont(fontGained);
+            setForeground(colorGained);
+        }
+        else {
+            setForeground(colorGained);
+            setFont(fontGained);
+            setText(getText());
+        }
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        if (getText().length() <= 0) {
+            setHint(getHint());
+        }
+        else {
+            setForeground(colorGained);
+            setFont(fontGained);
+            setText(getText());
+        }
+    }
+}
