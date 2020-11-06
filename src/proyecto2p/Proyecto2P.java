3c5d658a603b3728fcbc3277e74e0e7e985e390f
Priska-cmd <72408726+Priska-cmd@users.noreply.github.com>
2020-11-06 15:39:42 -0600

    Conferencias Inscritas LIsto
    Ya todo funciona

--- src/proyecto2p/Proyecto2P.java
+++ src/proyecto2p/Proyecto2P.java
@@ -2,11 +2,17 @@
 package proyecto2p;
 
 import BD.*;
+<<<<<<< Updated upstream
 import Models.usuarioGeneral;
 import Views.ConferenciasIns;
 import Views.Login;
 import Views.index;
 import Views.Menu;
+=======
+import Views.ConferenciasIns;
+
+import Views.NewIndex;
+>>>>>>> Stashed changes
 import Views.VConferencia;
 import java.sql.Connection;
 
@@ -17,7 +23,14 @@ public class Proyecto2P {
         conexion conex = new conexion();
         Connection cn = conex.getConnection();
         
+<<<<<<< Updated upstream
         new ConferenciasIns().setVisible(true);
+=======
+        new NewIndex().setVisible(true);
+        new ConferenciasIns().setVisible(true);
+
+        
+>>>>>>> Stashed changes
         
         new index().setVisible(true);
     }
